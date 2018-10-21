bc20bcab5ae7b3e1f99c78f92e44d370b98afbda
Maria Alavez <maria@devolute.org>
2018-10-21 22:58:22 +0200

    Add package.json

--- index.js
+++ index.js
@@ -2,28 +2,28 @@ const Retailer = require('./retailer')
 const Store = require('./store')
 const SaleEvent = require('./sale')
 const Customer = require('./customer')
+const Database = require('./database')
 
 
 
 
 // Create a Store
-var hundm = new Retailer("H & M")
+const hundm = new Retailer("H & M")
 
 // Create locations
-var hundm_alexa = new Store(hundm, "Alexa Berlin")
-var hundm_ring_center = new Store(hundm, "Ring-Center Berlin")
+const hundm_alexa = new Store(hundm, "Alexa Berlin")
+const hundm_ring_center = new Store(hundm, "Ring-Center Berlin")
 
 // Create a sale event
-var sale_30 = new SaleEvent(hundm_alexa, "Clothing", "30%")
+const sale_30 = new SaleEvent(hundm_alexa, "Clothing", "30%")
 
 // Create a customer
-var karl = new Customer("Karl")
+const karl = new Customer("Karl")
 
 // // Make the customer attend the sale event
 karl.attendSaleEvent(sale_30)
 
-console.log(hundm.stores[0].saleEvents)
-
+Database.save(hundm)
 
 
 
