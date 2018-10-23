8ac300a9541e2de85f14c16715a9cd6a4479face
Maria Alavez <maria@devolute.org>
2018-10-23 21:43:45 +0200

    Add CircularJSON to fix circular dependencies

--- database.js
+++ database.js
@@ -1,11 +1,12 @@
 const fs = require('fs')
+const CircularJSON = require('circular-json')
 
 module.exports = {
   save(data) {
-    fs.writeFileSync("data.json", JSON.stringify(data))
+    fs.writeFileSync("data.json", CircularJSON.stringify(data))
   },
 
   load() {
-    return JSON.parse(fs.readFileSync("data.json"))
+    return CircularJSON.parse(fs.readFileSync("data.json"))
   }
 }
--- index.js
+++ index.js
@@ -4,13 +4,10 @@ const SaleEvent = require('./sale')
 const Customer = require('./customer')
 const Database = require('./database')
 
-
-
-
-// Create a Store
+// Create a Retailer
 const hundm = new Retailer("H & M")
 
-// Create locations
+// Create Stores
 const hundm_alexa = new Store(hundm, "Alexa Berlin")
 const hundm_ring_center = new Store(hundm, "Ring-Center Berlin")
 
@@ -23,7 +20,11 @@ const karl = new Customer("Karl")
 // // Make the customer attend the sale event
 karl.attendSaleEvent(sale_30)
 
+// Save to DB
 Database.save(hundm)
+const loadedFile = Database.load()
+console.log(loadedFile.stores)
+// console.log(hundm)
 
 
 
